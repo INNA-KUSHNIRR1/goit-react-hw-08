eedba1f2de025322377fc00f98396f1449339fd5
INNA-KUSHNIRR1 <149315820+INNA-KUSHNIRR1@users.noreply.github.com>
2024-06-23 09:04:36 +0200

    change color of loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -8,12 +8,12 @@ const Loader = () => {
         visible={true}
         height="50"
         width="50"
-        color="grey"
+        strokeColor="grey"
         strokeWidth="5"
         animationDuration="0.75"
         ariaLabel="rotating-lines-loading"
         wrapperStyle={{}}
-        wrapperClass={style.loaderCss}
+        wrapperClass=""
       />
     </div>
   );
